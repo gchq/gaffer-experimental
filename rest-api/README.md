557420d16a8fa661cf7c84d8093afe1784fb98e2
“Go159lap” <go159lap@tutanota.com>
2021-02-10 15:59:18 +0000

    move rest api into gaffer experimental

---
+++ rest-api/README.md
@@ -0,0 +1,4 @@
+#How to run the REST API
+
+1. Log in to OpenShift using `oc login --token=[YOUR_TOKEN] --server=[YOUR_OPENSHIFT_SERVER]`
+2. Build and deploy using `mvn install`
---
+++ rest-api/src/main/java/com/example/springboottest/SpringbootTestApplication.java
@@ -0,0 +1,18 @@
+package com.example.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+
+	}
+
