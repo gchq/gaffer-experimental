04ac551cf0652f19d5415168dd7ec6f4d2e86c5e
Rocky <rocky341@protonmail.com>
2021-02-12 13:00:54 +0000

    checkstyle springbootTestApplication except for supression problem

--- gaffer-worker/src/main/java/uk/gov/gchq/gaffer/SpringbootTestApplication.java
+++ gaffer-worker/src/main/java/uk/gov/gchq/gaffer/SpringbootTestApplication.java
@@ -2,15 +2,14 @@ package uk.gov.gchq.gaffer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.*;
 
 
 @SpringBootApplication
 public class SpringbootTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTestApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
 
-	}
+}
 
