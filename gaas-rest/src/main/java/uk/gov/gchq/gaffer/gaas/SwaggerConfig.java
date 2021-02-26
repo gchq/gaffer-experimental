4e69079d5fa741ad13421dc716349a3e93c379a7
K-itKat20 <LaurelKatie@tutanota.com>
2021-02-26 15:06:06 +0000

    added stuff to pom and SwaggerConfig

--- gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/SwaggerConfig.java
+++ gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/SwaggerConfig.java
@@ -13,25 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package uk.gov.gchq.gaffer.gaas;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SpringFoxConfig {
+public class SwaggerConfig {                                    
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
 }
