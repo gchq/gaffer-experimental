2eb902dd544d50f1c09dde069de480eb01ad23a1
Rocky <rocky341@protonmail.com>
2021-02-26 15:54:48 +0000

    working

--- gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/SpringFoxConfig.java
+++ gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/SpringFoxConfig.java
@@ -14,26 +14,20 @@
  * limitations under the License.
  */
 package uk.gov.gchq.gaffer.gaas;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-public class SwaggerConfig {                                    
+public class SpringFoxConfig {
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
