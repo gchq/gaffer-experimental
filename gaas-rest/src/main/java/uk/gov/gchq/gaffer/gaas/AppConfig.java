03c17e40a77d7456278376dc1f7270c6726967de
K-itKat20 <LaurelKatie@tutanota.com>
2021-02-26 11:54:10 +0000

    Swagger Dependancies

--- gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/AppConfig.java
+++ gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/AppConfig.java
@@ -17,10 +17,15 @@ package uk.gov.gchq.gaffer.gaas;
 
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.util.ClientBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.gchq.gaffer.gaas.model.CRDClient;
 import java.io.IOException;
+
 
 @Configuration
 public class AppConfig {
@@ -33,5 +38,13 @@ public class AppConfig {
     @Bean
     public CRDClient crdClient() {
         return new CRDClient();
+    }
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
 }
