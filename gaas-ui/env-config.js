0140a8758e85587f2e36c14a9b0dcea2438afa5d
Rocky <rocky341@protonmail.com>
2021-03-09 11:53:22 +0000

    fixes to allow building of docker image

--- gaas-ui/env-config.js
+++ gaas-ui/env-config.js
@@ -1,5 +1,5 @@
-window.REACT_APP_API_PLATFORM="AWS"
-window.REACT_APP_KAI_REST_API_HOST="https://MY_API_LOCATION.execute-api.us-east-1.amazonaws.com/prod"
+window.REACT_APP_API_PLATFORM="test"
+window.REACT_APP_KAI_REST_API_HOST="http://gaas-rest-kai-helm-3.apps.ocp1.purplesky-harmony.co.uk"
 window.REACT_APP_COGNITO_USERPOOLID="us-east-1_ABC1def2g"
 window.REACT_APP_COGNITO_CLIENTID="42ov9u4hs69oji3y357aq26era"
 window.SKIP_PREFLIGHT_CHECK="true"
