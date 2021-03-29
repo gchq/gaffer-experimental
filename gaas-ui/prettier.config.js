ab0474f5c12503b3986687ab6704b8bdc6ce8dac
K-itKat20 <LaurelKatie@tutanota.com>
2021-03-29 12:20:23 +0100

    Changes to linting

--- gaas-ui/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    printWidth: 120,
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: true,
-    eslintIntegration: true,
-};
