23b8287c0225a6d2c190e9d4ebd204f98c417483
K-itKat20 <LaurelKatie@tutanota.com>
2021-03-29 12:32:40 +0100

    Minor Changes

--- gaas-ui/test/server/middleware.test.ts
+++ gaas-ui/test/server/middleware.test.ts
@@ -1,130 +1,130 @@
-import request from 'supertest';
-import server from '../../server/middleware';
+import request from "supertest";
+import server from "../../server/middleware";
 
 let token: string;
 
-afterEach(function () {
+afterEach(() => {
     server.close();
 });
 
-describe('Auth', () => {
-    it('Should respond to the POST method with a 200 status code when the username and password is correct', async () => {
+describe("Auth", () => {
+    it("Should respond to the POST method with a 200 status code when the username and password is correct", async () => {
         await request(server)
-            .post('/auth')
+            .post("/auth")
             .send({
-                username: 'user@yahoo.com',
-                password: 'abc123',
+                username: "user@yahoo.com",
+                password: "abc123",
             })
             .expect(200)
             .expect((res) => res.body !== undefined);
     });
-    it('Should respond with a 403 code when the POST method is called with the incorrect username and password', async () => {
+    it("Should respond with a 403 code when the POST method is called with the incorrect username and password", async () => {
         await request(server)
-            .post('/auth')
+            .post("/auth")
             .send({
-                username: 'invalidUser',
-                password: 'invalidPassword',
+                username: "invalidUser",
+                password: "invalidPassword",
             })
             .expect(403);
     });
-    it('Should respond with a 204 code when the POST method is called with the sign out path', async () => {
-        await request(server).post('/auth').send({
-            username: 'user@yahoo.com',
-            password: 'abc123',
+    it("Should respond with a 204 code when the POST method is called with the sign out path", async () => {
+        await request(server).post("/auth").send({
+            username: "user@yahoo.com",
+            password: "abc123",
         });
-        await request(server).post('/auth/signout').expect(204);
+        await request(server).post("/auth/signout").expect(204);
     });
 });
 
-describe('Graph API', () => {
+describe("Graph API", () => {
     beforeAll(async (done) => {
         await request(server)
-            .post('/auth')
+            .post("/auth")
             .send({
-                username: 'user@yahoo.com',
-                password: 'abc123',
+                username: "user@yahoo.com",
+                password: "abc123",
             })
             .then((response) => {
                 token = response.body;
                 done();
             });
     });
-    it('Should respond with a 201 code when post is called with the graphs path and user is signed in', async () => {
+    it("Should respond with a 201 code when post is called with the graphs path and user is signed in", async () => {
         await request(server)
-            .post('/graphs')
-            .set('Authorization', token)
+            .post("/graphs")
+            .set("Authorization", token)
             .send({
-                graphId: 'validGraph',
-                description: 'CREATING',
+                graphId: "validGraph",
+                description: "CREATING",
             })
             .expect(201);
     });
-    it('Should respond with a 500 code when post is called with the graphs path and user is signed in', async () => {
+    it("Should respond with a 500 code when post is called with the graphs path and user is signed in", async () => {
         await request(server)
-            .post('/graphs')
-            .set('Authorization', token)
+            .post("/graphs")
+            .set("Authorization", token)
             .send({
-                graphId: 'fail',
-                description: 'FAILED',
+                graphId: "fail",
+                description: "FAILED",
             })
             .expect(500);
     });
-    it('Should respond with graphs when GET is called with the graphs path and user is signed in', async () => {
+    it("Should respond with graphs when GET is called with the graphs path and user is signed in", async () => {
         await request(server)
-            .get('/graphs')
-            .set('Authorization', token)
+            .get("/graphs")
+            .set("Authorization", token)
             .then((response) => {
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toStrictEqual([
                     {
-                        graphId: 'roadTraffic',
-                        description: 'DEPLOYED',
+                        graphId: "roadTraffic",
+                        description: "DEPLOYED",
                     },
                     {
-                        graphId: 'basicGraph',
-                        description: 'DEPLOYED',
+                        graphId: "basicGraph",
+                        description: "DEPLOYED",
                     },
                 ]);
             });
     });
-    it('Should respond with a graph when GET is called with the graphs path and user is signed in', async () => {
+    it("Should respond with a graph when GET is called with the graphs path and user is signed in", async () => {
         await request(server)
-            .get('/graphs/roadTraffic')
-            .set('Authorization', token)
+            .get("/graphs/roadTraffic")
+            .set("Authorization", token)
             .then((response) => {
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toStrictEqual({
-                    graphId: 'roadTraffic',
-                    description: 'DEPLOYED',
+                    graphId: "roadTraffic",
+                    description: "DEPLOYED",
                 });
             });
     });
-    it('Should respond with a 204 code when GET is called with the graphs path and user is signed in', async () => {
+    it("Should respond with a 204 code when GET is called with the graphs path and user is signed in", async () => {
         await request(server)
-            .delete('/graphs/roadTraffic')
-            .set('Authorization', token)
+            .delete("/graphs/roadTraffic")
+            .set("Authorization", token)
             .then((response) => {
                 expect(response.statusCode).toBe(204);
             });
     });
 });
-describe('Namespaces', () => {
+describe("Namespaces", () => {
     beforeAll(async (done) => {
         await request(server)
-            .post('/auth')
+            .post("/auth")
             .send({
-                username: 'user@yahoo.com',
-                password: 'abc123',
+                username: "user@yahoo.com",
+                password: "abc123",
             })
             .then((response) => {
                 token = response.body;
                 done();
             });
     });
-    it('namespaces endpoint with Autorization token should return namespaces array', async () => {
+    it("namespaces endpoint with Autorization token should return namespaces array", async () => {
         await request(server)
-            .get('/namespaces')
-            .set('Authorization', token)
+            .get("/namespaces")
+            .set("Authorization", token)
             .then((response) => {
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toStrictEqual(["namespace1", "namespace2", "namespace3"]);
