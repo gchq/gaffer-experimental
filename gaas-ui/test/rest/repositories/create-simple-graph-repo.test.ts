40b524cc8b9c66c8c050e86c5a16abc19f3a90f4
K-itKat20 <LaurelKatie@tutanota.com>
2021-03-30 09:39:51 +0100

    Linting

--- gaas-ui/test/rest/repositories/create-simple-graph-repo.test.ts
+++ gaas-ui/test/rest/repositories/create-simple-graph-repo.test.ts
@@ -9,71 +9,67 @@ const mock = new MockAdapter(axios);
 const repo = new CreateSimpleGraphRepo();
 
 describe("Create Graph Repo", () => {
-  describe("On Success", () => {
-    it("should request an ACCUMULO store graph when ACCUMULO is paramete", async () => {
-      const request: ICreateSimpleGraphRequestBody = {
-        graphId: "accumulo-graph",
-        description: "a description",
-        storeType: StoreType.ACCUMULO,
-        url: "",
-        root: "",
-      };
-      mock.onPost("/graphs", request).reply(201);
+    describe("On Success", () => {
+        it("should request an ACCUMULO store graph when ACCUMULO is paramete", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "accumulo-graph",
+                description: "a description",
+                storeType: StoreType.ACCUMULO,
+                url: "",
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(201);
 
-      await expect(
-        repo.create("accumulo-graph", "a description", StoreType.ACCUMULO, "", "")
-      ).resolves.toEqual(undefined);
+            await expect(repo.create("accumulo-graph", "a description", StoreType.ACCUMULO, "", "")).resolves.toEqual(
+                undefined
+            );
+        });
+
+        it("should request an ACCUMULO store graph when MAPSTORE is paramete", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "map-graph",
+                description: "a description",
+                storeType: StoreType.MAPSTORE,
+                url: "",
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(201);
+
+            await expect(repo.create("map-graph", "a description", StoreType.MAPSTORE, "", "")).resolves.toEqual(
+                undefined
+            );
+        });
     });
 
-    it("should request an ACCUMULO store graph when MAPSTORE is paramete", async () => {
-      const request: ICreateSimpleGraphRequestBody = {
-        graphId: "map-graph",
-        description: "a description",
-        storeType: StoreType.MAPSTORE,
-        url: "",
-        root: "",
-      };
-      mock.onPost("/graphs", request).reply(201);
+    describe("On Error", () => {
+        it("should throw RestApiError 400 message when API no response body", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "bad-request-graph",
+                description: "a description",
+                storeType: StoreType.MAPSTORE,
+                url: "",
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(400);
 
-      await expect(
-        repo.create("map-graph", "a description", StoreType.MAPSTORE, "", "")
-      ).resolves.toEqual(undefined);
+            await expect(repo.create("bad-request-graph", "a description", StoreType.MAPSTORE, "", "")).rejects.toEqual(
+                new RestApiError("Error Code 400", "Bad Request")
+            );
+        });
+
+        it("should throw RestApiError with title and detail from error response body", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "forbidden-graph",
+                description: "a description",
+                storeType: StoreType.MAPSTORE,
+                url: "",
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(403, { title: "Forbidden", detail: "Kubernetes access denied" });
+
+            await expect(repo.create("forbidden-graph", "a description", StoreType.MAPSTORE, "", "")).rejects.toEqual(
+                new RestApiError("Forbidden", "Kubernetes access denied")
+            );
+        });
     });
-  });
-
-  describe("On Error", () => {
-    it("should throw RestApiError 400 message when API no response body", async () => {
-      const request: ICreateSimpleGraphRequestBody = {
-        graphId: "bad-request-graph",
-        description: "a description",
-        storeType: StoreType.MAPSTORE,
-        url: "",
-        root: "",
-      };
-      mock.onPost("/graphs", request).reply(400);
-
-      await expect(
-        repo.create("bad-request-graph", "a description", StoreType.MAPSTORE, "", "")
-      ).rejects.toEqual(new RestApiError("Error Code 400", "Bad Request"));
-    });
-
-    it("should throw RestApiError with title and detail from error response body", async () => {
-      const request: ICreateSimpleGraphRequestBody = {
-        graphId: "forbidden-graph",
-        description: "a description",
-        storeType: StoreType.MAPSTORE,
-        url: "",
-        root: "",
-      };
-      mock
-        .onPost("/graphs", request)
-        .reply(403, { title: "Forbidden", detail: "Kubernetes access denied" });
-
-      await expect(
-        repo.create("forbidden-graph", "a description", StoreType.MAPSTORE, "", "")
-      ).rejects.toEqual(
-        new RestApiError("Forbidden", "Kubernetes access denied")
-      );
-    });
-  });
 });
--- gaas-ui/test/rest/repositories/get-all-namespaces-repo.test.ts
+++ gaas-ui/test/rest/repositories/get-all-namespaces-repo.test.ts
@@ -10,53 +10,41 @@ const repo = new GetAllNamespacesRepo();
 afterEach(() => mock.resetHandlers());
 
 describe("Get All Namespaces Repo", () => {
-  it("should return many namespaces when api returns many", async () => {
-    const apiResponse: IAllNameSpacesResponse = [
-      "namespace1",
-      "namespace2",
-      "namespace3",
-    ];
+    it("should return many namespaces when api returns many", async () => {
+        const apiResponse: IAllNameSpacesResponse = ["namespace1", "namespace2", "namespace3"];
 
-    mock.onGet("/namespaces").reply(200, apiResponse);
+        mock.onGet("/namespaces").reply(200, apiResponse);
 
-    const actual: Array<string> = await repo.getAll();
+        const actual: Array<string> = await repo.getAll();
 
-    const expected = ["namespace1", "namespace2", "namespace3"];
-    expect(actual).toEqual(expected);
-  });
+        const expected = ["namespace1", "namespace2", "namespace3"];
+        expect(actual).toEqual(expected);
+    });
 
-  it("should return one namespace when the api returns one", async () => {
-    const apiResponse: IAllNameSpacesResponse = ["namespace1"];
+    it("should return one namespace when the api returns one", async () => {
+        const apiResponse: IAllNameSpacesResponse = ["namespace1"];
 
-    mock.onGet("/namespaces").reply(200, apiResponse);
+        mock.onGet("/namespaces").reply(200, apiResponse);
 
-    const actual: Array<string> = await repo.getAll();
+        const actual: Array<string> = await repo.getAll();
 
-    const expected = ["namespace1"];
-    expect(actual).toEqual(expected);
-  });
-  it("should throw RestApiError with correct status message when no response body", async () => {
-    mock.onGet("/namespaces").reply(404);
+        const expected = ["namespace1"];
+        expect(actual).toEqual(expected);
+    });
+    it("should throw RestApiError with correct status message when no response body", async () => {
+        mock.onGet("/namespaces").reply(404);
 
-    await expect(repo.getAll()).rejects.toEqual(
-      new RestApiError("Error Code 404", "Not Found")
-    );
-  });
-  it("should throw RestApiError with title and detail from response body", async () => {
-    mock
-      .onGet("/namespaces")
-      .reply(404, { title: "Forbidden", detail: "Kubernetes access denied" });
+        await expect(repo.getAll()).rejects.toEqual(new RestApiError("Error Code 404", "Not Found"));
+    });
+    it("should throw RestApiError with title and detail from response body", async () => {
+        mock.onGet("/namespaces").reply(404, { title: "Forbidden", detail: "Kubernetes access denied" });
 
-    await expect(repo.getAll()).rejects.toEqual(
-      new RestApiError("Forbidden", "Kubernetes access denied")
-    );
-  });
+        await expect(repo.getAll()).rejects.toEqual(new RestApiError("Forbidden", "Kubernetes access denied"));
+    });
 
-  it("should throw unknown RestApiError when undefined status and body", async () => {
-    mock.onGet("/namespaces").reply(0);
+    it("should throw unknown RestApiError when undefined status and body", async () => {
+        mock.onGet("/namespaces").reply(0);
 
-    await expect(repo.getAll()).rejects.toEqual(
-      new RestApiError("Unknown Error", "Unable to make request")
-    );
-  });
+        await expect(repo.getAll()).rejects.toEqual(new RestApiError("Unknown Error", "Unable to make request"));
+    });
 });
