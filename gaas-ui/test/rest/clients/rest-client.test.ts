c44b05f0d9e1c689afe17a9133d700ee1fa2de1a
K-itKat20 <LaurelKatie@tutanota.com>
2021-03-30 09:39:51 +0100

    Linting

--- gaas-ui/test/rest/clients/rest-client.test.ts
+++ gaas-ui/test/rest/clients/rest-client.test.ts
@@ -5,120 +5,109 @@ import { RestClient } from "../../../src/rest/clients/rest-client";
 const mock = new MockAdapter(axios);
 
 describe("RestClient 2** Responses", () => {
-  beforeAll(() =>
-    mock
-      .onGet("/graphs")
-      .reply(200, [{ graphId: "any-graph", currentStatus: "DEPLOYED" }])
-      .onGet("/graphs/graph-1")
-      .reply(200, { graphId: "graph-1", currentStatus: "DELETED" })
-      .onPost("/graphs", { post: "this" })
-      .reply(201)
-      .onDelete("/graphs/redundant-graph")
-      .reply(202)
-  );
-  afterAll(() => mock.resetHandlers());
+    beforeAll(() =>
+        mock
+            .onGet("/graphs")
+            .reply(200, [{ graphId: "any-graph", currentStatus: "DEPLOYED" }])
+            .onGet("/graphs/graph-1")
+            .reply(200, { graphId: "graph-1", currentStatus: "DELETED" })
+            .onPost("/graphs", { post: "this" })
+            .reply(201)
+            .onDelete("/graphs/redundant-graph")
+            .reply(202)
+    );
+    afterAll(() => mock.resetHandlers());
 
-  it("should return status/data when GET is successful", async () => {
-    const actual = await new RestClient().get().graphs().execute();
+    it("should return status/data when GET is successful", async () => {
+        const actual = await new RestClient().get().graphs().execute();
 
-    expect(actual).toEqual({
-      status: 200,
-      data: [
-        {
-          graphId: "any-graph",
-          currentStatus: "DEPLOYED",
-        },
-      ],
+        expect(actual).toEqual({
+            status: 200,
+            data: [
+                {
+                    graphId: "any-graph",
+                    currentStatus: "DEPLOYED",
+                },
+            ],
+        });
     });
-  });
-  it("should return status/data when GET with a path variable is successful", async () => {
-    const actual = await new RestClient().get().graphs("graph-1").execute();
+    it("should return status/data when GET with a path variable is successful", async () => {
+        const actual = await new RestClient().get().graphs("graph-1").execute();
 
-    expect(actual).toEqual({
-      status: 200,
-      data: {
-        graphId: "graph-1",
-        currentStatus: "DELETED",
-      },
+        expect(actual).toEqual({
+            status: 200,
+            data: {
+                graphId: "graph-1",
+                currentStatus: "DELETED",
+            },
+        });
     });
-  });
-  it("should return status when POST with request body is successful", async () => {
-    const actual = await new RestClient()
-      .post()
-      .graphs()
-      .requestBody({ post: "this" })
-      .execute();
+    it("should return status when POST with request body is successful", async () => {
+        const actual = await new RestClient().post().graphs().requestBody({ post: "this" }).execute();
 
-    expect(actual).toEqual({
-      status: 201,
+        expect(actual).toEqual({
+            status: 201,
+        });
     });
-  });
-  it("should return status when DELETE with path variable is successful", async () => {
-    const actual = await new RestClient()
-      .delete()
-      .graphs("redundant-graph")
-      .execute();
+    it("should return status when DELETE with path variable is successful", async () => {
+        const actual = await new RestClient().delete().graphs("redundant-graph").execute();
 
-    expect(actual).toEqual({
-      status: 202,
+        expect(actual).toEqual({
+            status: 202,
+        });
     });
-  });
 });
 
 describe("RestClient 4**/5** Error Responses", () => {
-  beforeAll(() =>
-    mock
-      .onGet("/graphs")
-      .reply(400, {
-        title: "Validation Failed",
-        detail: "Graph ID can not be null",
-      })
-      .onGet("/graphs/unfindable-graph")
-      .reply(404, { title: "Not Found", detail: "Could not find resource" })
-      .onPost("/graphs", { request: "not-found" })
-      .reply(500, {
-        title: "Server Error",
-        detail: "Null pointer in back end API",
-      })
-      .onDelete("/graphs/already-deleted")
-      .reply(504, { title: "Server Error", detail: "Timeout" })
-  );
-  afterAll(() => mock.resetHandlers());
+    beforeAll(() =>
+        mock
+            .onGet("/graphs")
+            .reply(400, {
+                title: "Validation Failed",
+                detail: "Graph ID can not be null",
+            })
+            .onGet("/graphs/unfindable-graph")
+            .reply(404, { title: "Not Found", detail: "Could not find resource" })
+            .onPost("/graphs", { request: "not-found" })
+            .reply(500, {
+                title: "Server Error",
+                detail: "Null pointer in back end API",
+            })
+            .onDelete("/graphs/already-deleted")
+            .reply(504, { title: "Server Error", detail: "Timeout" })
+    );
+    afterAll(() => mock.resetHandlers());
 
-  it("should throw 400 Error Message when api returns 404", async () => {
-    try {
-      await new RestClient().get().graphs().execute();
-      throw new Error("Error did not throw");
-    } catch (e) {
-      expect(e.toString()).toBe("Validation Failed: Graph ID can not be null");
-    }
-  });
-  it("should throw 404 Error Message when api returns 404", async () => {
-    try {
-      await new RestClient().get().graphs("unfindable-graph").execute();
-      throw new Error("Error did not throw");
-    } catch (e) {
-      expect(e.toString()).toBe("Not Found: Could not find resource");
-    }
-  });
-  it("should throw 500 Error Message when api returns 404", async () => {
-    try {
-      await new RestClient()
-        .post()
-        .graphs()
-        .requestBody({ request: "not-found" })
-        .execute();
-      throw new Error("Error did not throw");
-    } catch (e) {
-      expect(e.toString()).toBe("Server Error: Null pointer in back end API");
-    }
-  });
-  it("should throw 504 Error Message when api returns 404", async () => {
-    try {
-      await new RestClient().delete().graphs("already-deleted").execute();
-      throw new Error("Error did not throw");
-    } catch (e) {
-      expect(e.toString()).toBe("Server Error: Timeout");
-    }
-  });
+    it("should throw 400 Error Message when api returns 404", async () => {
+        try {
+            await new RestClient().get().graphs().execute();
+            throw new Error("Error did not throw");
+        } catch (e) {
+            expect(e.toString()).toBe("Validation Failed: Graph ID can not be null");
+        }
+    });
+    it("should throw 404 Error Message when api returns 404", async () => {
+        try {
+            await new RestClient().get().graphs("unfindable-graph").execute();
+            throw new Error("Error did not throw");
+        } catch (e) {
+            expect(e.toString()).toBe("Not Found: Could not find resource");
+        }
+    });
+    it("should throw 500 Error Message when api returns 404", async () => {
+        try {
+            await new RestClient().post().graphs().requestBody({ request: "not-found" }).execute();
+            throw new Error("Error did not throw");
+        } catch (e) {
+            expect(e.toString()).toBe("Server Error: Null pointer in back end API");
+        }
+    });
+    it("should throw 504 Error Message when api returns 404", async () => {
+        try {
+            await new RestClient().delete().graphs("already-deleted").execute();
+            throw new Error("Error did not throw");
+        } catch (e) {
+            expect(e.toString()).toBe("Server Error: Timeout");
+        }
+    });
 });
--- gaas-ui/test/rest/repositories/create-graph-repo.test.ts
+++ gaas-ui/test/rest/repositories/create-graph-repo.test.ts
@@ -9,69 +9,55 @@ const mock = new MockAdapter(axios);
 const repo = new CreateGraphRepo();
 
 describe("Create Graph Repo", () => {
-  describe("On Success", () => {
-    it("should called with ", async () => {
-      mock.onPost("/graphs").reply(201);
-      const elements = new ElementsSchema(
-        JSON.stringify({ entities: {}, edges: {} })
-      );
-      const types = new TypesSchema(JSON.stringify({ types: {} }));
+    describe("On Success", () => {
+        it("should called with ", async () => {
+            mock.onPost("/graphs").reply(201);
+            const elements = new ElementsSchema(JSON.stringify({ entities: {}, edges: {} }));
+            const types = new TypesSchema(JSON.stringify({ types: {} }));
 
-      await expect(repo.create("ok", [], elements, types)).resolves.toEqual(
-        undefined
-      );
+            await expect(repo.create("ok", [], elements, types)).resolves.toEqual(undefined);
+        });
+
+        it("should called with ", async () => {
+            mock.onPost("/graphs").reply(200);
+            const elements = new ElementsSchema(JSON.stringify({ entities: {}, edges: {} }));
+            const types = new TypesSchema(JSON.stringify({ types: {} }));
+
+            await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
+                new Error("Expected status code 201 for Created Graph but got (200)")
+            );
+        });
     });
 
-    it("should called with ", async () => {
-      mock.onPost("/graphs").reply(200);
-      const elements = new ElementsSchema(
-        JSON.stringify({ entities: {}, edges: {} })
-      );
-      const types = new TypesSchema(JSON.stringify({ types: {} }));
+    describe("On Error", () => {
+        it("should throw RestApiError 400 message when API no response body", async () => {
+            mock.onPost("/graphs").reply(400);
+            const elements = new ElementsSchema(JSON.stringify({ entities: {}, edges: {} }));
+            const types = new TypesSchema(JSON.stringify({ types: {} }));
 
-      await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
-        new Error("Expected status code 201 for Created Graph but got (200)")
-      );
+            await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
+                new RestApiError("Error Code 400", "Bad Request")
+            );
+        });
+
+        it("should throw RestApiError with title and detail from response body", async () => {
+            mock.onPost("/graphs").reply(403, { title: "Forbidden", detail: "Kubernetes access denied" });
+            const elements = new ElementsSchema(JSON.stringify({ entities: {}, edges: {} }));
+            const types = new TypesSchema(JSON.stringify({ types: {} }));
+
+            await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
+                new RestApiError("Forbidden", "Kubernetes access denied")
+            );
+        });
+
+        it("should throw Unknown RestApiError when no error and response body", async () => {
+            mock.onPost("/graphs").reply(0);
+            const elements = new ElementsSchema(JSON.stringify({ entities: {}, edges: {} }));
+            const types = new TypesSchema(JSON.stringify({ types: {} }));
+
+            await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
+                new RestApiError("Unknown Error", "Unable to make request")
+            );
+        });
     });
-  });
-
-  describe("On Error", () => {
-    it("should throw RestApiError 400 message when API no response body", async () => {
-      mock.onPost("/graphs").reply(400);
-      const elements = new ElementsSchema(
-        JSON.stringify({ entities: {}, edges: {} })
-      );
-      const types = new TypesSchema(JSON.stringify({ types: {} }));
-
-      await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
-        new RestApiError("Error Code 400", "Bad Request")
-      );
-    });
-
-    it("should throw RestApiError with title and detail from response body", async () => {
-      mock
-        .onPost("/graphs")
-        .reply(403, { title: "Forbidden", detail: "Kubernetes access denied" });
-      const elements = new ElementsSchema(
-        JSON.stringify({ entities: {}, edges: {} })
-      );
-      const types = new TypesSchema(JSON.stringify({ types: {} }));
-
-      await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
-        new RestApiError("Forbidden", "Kubernetes access denied")
-      );
-    });
-
-    it("should throw Unknown RestApiError when no error and response body", async () => {
-      mock.onPost("/graphs").reply(0);
-      const elements = new ElementsSchema(
-        JSON.stringify({ entities: {}, edges: {} })
-      );
-      const types = new TypesSchema(JSON.stringify({ types: {} }));
-
-      await expect(repo.create("bad", [], elements, types)).rejects.toEqual(
-        new RestApiError("Unknown Error", "Unable to make request")
-      );
-    });
-  });
 });
