85c7bc6cf40e6dceec4c2405d7b8d47dbc78e30d
K-itKat20 <LaurelKatie@tutanota.com>
2021-04-12 16:05:21 +0100

    Added status to view graph

--- gaas-ui/src/domain/graph.ts
+++ gaas-ui/src/domain/graph.ts
@@ -2,21 +2,26 @@ export class Graph {
     private graphId: string;
     private description: string;
     private url: string
+    private status: string
 
-    constructor(graphId: string, description: string, url: string) {
+    constructor(graphId: string, description: string, url: string, status: string) {
         this.graphId = graphId;
         this.description = description;
         this.url = url;
+        this.status = status;
     }
 
     public getId(): string {
         return this.graphId;
     }
 
-    public getStatus(): string {
+    public getDescription(): string {
         return this.description;
     }
     public getUrl(): string {
         return this.url;
+    }
+    public getStatus(): string {
+        return this.status;
     }
 }
