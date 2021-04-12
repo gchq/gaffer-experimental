6b8f0ac609bbcdc7a7a7f7121acc5c3245d0aa43
Rocky <rocky341@protonmail.com>
2021-04-12 14:46:48 +0100

    create simple graph repo fixing tests / making new ones

--- gaas-ui/test/rest/repositories/create-simple-graph-repo.test.ts
+++ gaas-ui/test/rest/repositories/create-simple-graph-repo.test.ts
@@ -4,39 +4,68 @@ import axios from "axios";
 import { RestApiError } from "../../../src/rest/RestApiError";
 import { ICreateSimpleGraphRequestBody } from "../../../src/rest/http-message-interfaces/request-interfaces";
 import { StoreType } from "../../../src/domain/store-type";
+import { Graph } from "../../../src/domain/graph";
 
 const mock = new MockAdapter(axios);
 const repo = new CreateSimpleGraphRepo();
 
 describe("Create Graph Repo", () => {
     describe("On Success", () => {
-        it("should request an ACCUMULO store graph when ACCUMULO is paramete", async () => {
+        it("should request an ACCUMULO store graph when ACCUMULO is parameter", async () => {
             const request: ICreateSimpleGraphRequestBody = {
                 graphId: "accumulo-graph",
                 description: "a description",
                 storeType: StoreType.ACCUMULO,
-                proxyStores: "",
+                proxyStores: [],
                 root: "",
             };
             mock.onPost("/graphs", request).reply(201);
 
-            await expect(repo.create("accumulo-graph", "a description", StoreType.ACCUMULO, "", "")).resolves.toEqual(
+            await expect(repo.create("accumulo-graph", "a description", StoreType.ACCUMULO, [], "")).resolves.toEqual(
                 undefined
             );
         });
 
-        it("should request an ACCUMULO store graph when MAPSTORE is paramete", async () => {
+        it("should request an MAPSTORE  graph when MAPSTORE is parameter", async () => {
             const request: ICreateSimpleGraphRequestBody = {
                 graphId: "map-graph",
                 description: "a description",
                 storeType: StoreType.MAPSTORE,
-                proxyStores: "",
+                proxyStores: [],
                 root: "",
             };
             mock.onPost("/graphs", request).reply(201);
 
-            await expect(repo.create("map-graph", "a description", StoreType.MAPSTORE, "", "")).resolves.toEqual(
+            await expect(repo.create("map-graph", "a description", StoreType.MAPSTORE, [], "")).resolves.toEqual(
                 undefined
+            );
+        });
+        it("should request an FEDERATED_STORE  graph when FEDERATED_STORE is parameter", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "fed-store",
+                description: "a description",
+                storeType: StoreType.FEDERATED_STORE,
+                proxyStores: [],
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(201);
+
+            await expect(repo.create("fed-store", "a description", StoreType.FEDERATED_STORE, [], "")).resolves.toEqual(
+                undefined
+            );
+        });
+        it("should request an FEDERATED_STORE  graph when FEDERATED_STORE is parameter", async () => {
+            const request: ICreateSimpleGraphRequestBody = {
+                graphId: "fed-store",
+                description: "a description",
+                storeType: StoreType.FEDERATED_STORE,
+                proxyStores: [new Graph("proxy-graph","description","proxy-graph URL")],
+                root: "",
+            };
+            mock.onPost("/graphs", request).reply(201);
+
+            await expect(repo.create("map-graph", "a description", StoreType.MAPSTORE, [], "")).resolves.toEqual(
+                request
             );
         });
     });
@@ -47,12 +76,12 @@ describe("Create Graph Repo", () => {
                 graphId: "bad-request-graph",
                 description: "a description",
                 storeType: StoreType.MAPSTORE,
-                proxyStores: "",
+                proxyStores: [],
                 root: "",
             };
             mock.onPost("/graphs", request).reply(400);
 
-            await expect(repo.create("bad-request-graph", "a description", StoreType.MAPSTORE, "", "")).rejects.toEqual(
+            await expect(repo.create("bad-request-graph", "a description", StoreType.MAPSTORE,[], "")).rejects.toEqual(
                 new RestApiError("Error Code 400", "Bad Request")
             );
         });
@@ -62,12 +91,12 @@ describe("Create Graph Repo", () => {
                 graphId: "forbidden-graph",
                 description: "a description",
                 storeType: StoreType.MAPSTORE,
-                proxyStores: "",
+                proxyStores: [],
                 root: "",
             };
             mock.onPost("/graphs", request).reply(403, { title: "Forbidden", detail: "Kubernetes access denied" });
 
-            await expect(repo.create("forbidden-graph", "a description", StoreType.MAPSTORE, "", "")).rejects.toEqual(
+            await expect(repo.create("forbidden-graph", "a description", StoreType.MAPSTORE, [], "")).rejects.toEqual(
                 new RestApiError("Forbidden", "Kubernetes access denied")
             );
         });
