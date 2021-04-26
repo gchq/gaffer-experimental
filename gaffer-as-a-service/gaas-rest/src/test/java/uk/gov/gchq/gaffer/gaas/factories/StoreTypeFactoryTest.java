f6637a2ecda672ada7b1e547d3ef90cfaf4a6850
go159lab <Go159Lab@tutaNota.com>
2021-04-26 11:39:45 +0100

    add junit tests

---
+++ gaffer-as-a-service/gaas-rest/src/test/java/uk/gov/gchq/gaffer/gaas/factories/StoreTypeFactoryTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.gaas.factories;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.gchq.gaffer.controller.model.v1.GafferSpec;
+import uk.gov.gchq.gaffer.gaas.model.GaaSCreateRequestBody;
+import uk.gov.gchq.gaffer.gaas.utilities.UnitTest;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@UnitTest
+class StoreTypeFactoryTest {
+
+  @Test
+  void testGetProxyStoreBuilder() {
+    List<StoreType> storeTypeManager = new ArrayList<>();
+
+    setStoreList(storeTypeManager);
+
+    GaaSCreateRequestBody gaaSCreateRequestBody = new GaaSCreateRequestBody("mygraph", "Another description", "proxyStore", null, "http://my.graph.co.uk", null);
+
+
+    StoreTypeFactory storeTypeFactory = new StoreTypeFactory(storeTypeManager);
+    AbstractStoreTypeBuilder builder = storeTypeFactory.getBuilder(gaaSCreateRequestBody);
+    final String expected =
+            "{graph={storeProperties={gaffer.host=http://my.graph.co.uk, gaffer.store.class=uk.gov.gchq.gaffer.proxystore.ProxyStore}, config={description=Another description, graphId=mygraph}}, ingress={host=mygraph-kai-dev.apps.my.kubernetes.cluster, pathPrefix={ui=/ui, api=/rest}}}";
+
+    GafferSpec build = builder.setGraphId("mygraph").setDescription("Another description").build();
+    assertEquals(expected, build.toString());
+  }
+
+  private void setStoreList(List<StoreType> storeTypeManager) {
+    MapStoreType mapStore = new MapStoreType();
+    ProxyStoreType proxyStoreType = new ProxyStoreType();
+    storeTypeManager.add(mapStore);
+    storeTypeManager.add(proxyStoreType);
+  }
+
+
+  @Test
+  void testGetMapStoreBuilder() {
+    List<StoreType> storeTypeManager = new ArrayList<>();
+
+    setStoreList(storeTypeManager);
+
+    GaaSCreateRequestBody gaaSCreateRequestBody = new GaaSCreateRequestBody("mygraph", "Another description", "mapStore", getSchema());
+
+    StoreTypeFactory storeTypeFactory = new StoreTypeFactory(storeTypeManager);
+    AbstractStoreTypeBuilder builder = storeTypeFactory.getBuilder(gaaSCreateRequestBody);
+    final String expected ="{graph={schema={schema.json={\"entities\":{},\"edges\":{},\"types\":{}}}, storeProperties={gaffer.store.job.tracker.enabled=true, gaffer.cache.service.class=uk.gov.gchq.gaffer.cache.impl.HashMapCacheService}, config={description=Another description, graphId=mygraph}}, ingress={host=mygraph-kai-dev.apps.my.kubernetes.cluster, pathPrefix={ui=/ui, api=/rest}}}";
+
+    GafferSpec build = builder.setGraphId("mygraph").setDescription("Another description").build();
+    assertEquals(expected, build.toString());
+  }
+
+  @Test
+  void testGetInvalidStoreBuilder() {
+    List<StoreType> storeTypeManager = new ArrayList<>();
+
+    setStoreList(storeTypeManager);
+
+    GaaSCreateRequestBody gaaSCreateRequestBody = new GaaSCreateRequestBody("mygraph", "Another description", "invalidStore", getSchema());
+    StoreTypeFactory storeTypeFactory = new StoreTypeFactory(storeTypeManager);
+    final RuntimeException exception = assertThrows(RuntimeException.class, () -> storeTypeFactory.getBuilder(gaaSCreateRequestBody));
+
+    final String expected ="java.lang.RuntimeException: StoreType is Invalid must be defined Valid Store Types supported are: federatedStore, accumuloStore, proxyStore and mapStore";
+
+    assertEquals(expected, exception.toString());
+  }
+
+  private LinkedHashMap<String, Object> getSchema() {
+    final LinkedHashMap<String, Object> elementsSchema = new LinkedHashMap<>();
+    elementsSchema.put("entities", new Object());
+    elementsSchema.put("edges", new Object());
+    elementsSchema.put("types", new Object());
+    return elementsSchema;
+  }
+}
