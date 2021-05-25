05c5689714c44bb80bf56325a4fcca4b4489879e
Rocky <rocky341@protonmail.com>
2021-05-25 11:07:16 +0100

    accessibility fixes

--- gaas-ui/src/components/user-guide/user-guide.tsx
+++ gaas-ui/src/components/user-guide/user-guide.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, Grid, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardActions, CardContent, Grid, makeStyles, Toolbar, Typography } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import React from "react";
 import ReactJson from "react-json-view";
@@ -29,76 +29,83 @@ export default class UserGuide extends React.Component<{}, {}> {
                             <Typography gutterBottom variant="h5" component="h2">
                                 Add Graphs
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 When creating a new graph you need a unique Graph Name, an Elements Schema and a Types
-                                Schema. Type in a unique name in the Graph Name text field.
+                                Schema. Type in a unique name in the Graph Name text field. 
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Type in the elements in the Schema Elements JSON textarea.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Type in the types in the Schema Types JSON textarea.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 You can import elements and types by clicking the document icon. For either elements or
                                 types, you can only import a single JSON file. You can remove your uploaded files by
                                 clicking on the clear icon next to the name of your file in the selected files section.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Once you exit the dialog box, your imported elements and types will appear in the
                                 elements and types textareas. You can edit the imported elements or types by typing in
                                 the elements or types textarea.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Click Add Graph to add your graph. If your elements or types are invalid, it will give
                                 you an error.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Note: Make sure your elements and types are surrounded by curly brackets(
                                 {"{}"}).
                             </Typography>
-                        </CardContent>
-
-                        <CardContent>
+                            <Box my={3} />
                             <Typography gutterBottom variant="h5" component="h2">
                                 View Graphs
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 View your graphs in the View Graphs section.
                             </Typography>
-                        </CardContent>
-
-                        <CardContent>
+                            <Box my={3} />
                             <Typography gutterBottom variant="h5" component="h2">
                                 Elements and Types
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 An Elements Schema and a Types Schema is merged to form a Full Schema.
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Example Schema Elements:
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                The Elements schema is made of edges and entities, as shown below.
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
+                                The Elements schema is made of edges and entities: 
                             </Typography>
+                            <Box my={1} />
                             <div id="example-elements-schema">
                                 <ReactJson
                                     src={exampleElementsSchema}
-                                    theme="summerfruit:inverted"
+                                    theme="bright"
                                     displayDataTypes={false}
                                     displayObjectSize={false}
                                 />
                             </div>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
                                 Example Schema Types:
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                The Types schema is made of types, as shown below.
+                            <Box my={1} />
+                            <Typography variant="body1" style={{color:"#0000000"}} component="p">
+                                The Types schema is made of types:
                             </Typography>
+                            <Box my={1} />
                             <div id="example-types-schema">
                                 <ReactJson
                                     src={exampleTypesSchema}
-                                    theme="summerfruit:inverted"
+                                    theme="bright"
                                     displayDataTypes={false}
                                     displayObjectSize={false}
                                 />
