a5277f3d265d0189c20169fc0878b63688e95b4d
Rocky <rocky341@protonmail.com>
2021-06-04 10:12:04 +0100

    refactored propertiesLoader

--- gaffer-as-a-service/gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/services/CreateGraphService.java
+++ gaffer-as-a-service/gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/services/CreateGraphService.java
@@ -17,74 +17,31 @@
 package uk.gov.gchq.gaffer.gaas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.stereotype.Service;
-import org.yaml.snakeyaml.Yaml;
 import uk.gov.gchq.gaffer.common.model.v1.Gaffer;
 import uk.gov.gchq.gaffer.gaas.client.CRDClient;
 import uk.gov.gchq.gaffer.gaas.exception.GaaSRestApiException;
 import uk.gov.gchq.gaffer.gaas.model.GaaSCreateRequestBody;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Map;
+import uk.gov.gchq.gaffer.gaas.util.PropertiesLoader;
 import static uk.gov.gchq.gaffer.gaas.factories.GafferHelmValuesFactory.from;
 
 @Service
 public class CreateGraphService {
-  @Autowired
-  private ResourceLoader resourceLoader;
+    @Autowired
+    private ResourceLoader resourceLoader;
 
-  @Autowired
-  private CRDClient crdClient;
+    @Autowired
+    private CRDClient crdClient;
 
-  public void createGraph(final GaaSCreateRequestBody gaaSCreateRequestBodyInput) throws GaaSRestApiException {
-    crdClient.createCRD(makeGafferHelmValues(gaaSCreateRequestBodyInput));
-  }
-
-  private Gaffer makeGafferHelmValues(final GaaSCreateRequestBody graph) throws GaaSRestApiException {
-    loadStoreProperties(graph);
-    return from(graph);
-  }
-
-  private void loadStoreProperties(final GaaSCreateRequestBody graph) throws GaaSRestApiException {
-    String storeType = graph.getStoreType();
-    Yaml yaml = new Yaml();
-    try {
-      InputStream inputStream = this.getClass()
-              .getClassLoader()
-              .getResourceAsStream("yaml/" + storeType + ".yaml");
-      Map<String, Object> storeProperties = yaml.load(inputStream);
-      graph.setStoreProperties(storeProperties);
-
-
-    } catch (Exception e) {
-      throw new RuntimeException("StoreType is Invalid must be defined Valid Store Types supported are: " + getStoreTypesAsStringList());
+    public void createGraph(final GaaSCreateRequestBody gaaSCreateRequestBodyInput) throws GaaSRestApiException {
+        crdClient.createCRD(makeGafferHelmValues(gaaSCreateRequestBodyInput));
     }
 
-  }
-
-  private String getStoreTypesAsStringList() throws GaaSRestApiException {
-    ArrayList<String> storeTypes = new ArrayList<>();
-    Resource[] resources = loadResources("classpath*:yaml/*.yaml");
-    for (final Resource resource : resources) {
-      String filename = resource.getFilename().split("\\.", 2)[0];
-      storeTypes.add(filename);
+    private Gaffer makeGafferHelmValues(final GaaSCreateRequestBody graph) throws GaaSRestApiException {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        propertiesLoader.loadStoreProperties(graph);
+        return from(graph);
     }
-    String str = storeTypes.toString().replaceAll("\\[|\\]", "");
-    return str;
-  }
-
-  private Resource[] loadResources(final String pattern) throws GaaSRestApiException {
-    try {
-      return ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources(pattern);
-    } catch (IOException e) {
-      throw new GaaSRestApiException(e.getLocalizedMessage(), e.getMessage(), 500);
-    }
-
-  }
-
 
 }
