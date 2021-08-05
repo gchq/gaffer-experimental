37f4a5d6dadd9f6262637f275a55462af2a450fb
Rocky <rocky341@protonmail.com>
2021-08-05 12:37:57 +0100

    restClient changes

--- gaas-ui/src/rest/clients/rest-client.ts
+++ gaas-ui/src/rest/clients/rest-client.ts
@@ -39,81 +39,81 @@ export class RestClient<T> {
     private methodSpec = (restClient: RestClient<any>) => ({
         get: () => { 
         restClient.method = "get";
-        return restClient.uriSpec;
+        return restClient.uriSpec(restClient);
          },
         post: () => { 
             restClient.method = "post";
-            return restClient.requestBodySpec;
+            return restClient.requestBodySpec(restClient);
         },
         delete: () => { 
             restClient.method = "delete";
-            return restClient.uriSpec;
+            return restClient.uriSpec(restClient);
         }
     })
 
-    private requestBodySpec = {
+    private requestBodySpec = (restClient: RestClient<any>) => ({
         requestBody: (requestBody?: T) => {
-            this.data = requestBody;
-            return this.uriSpec;
+            restClient.data = requestBody;
+            return restClient.uriSpec(restClient);
         }
-    }
+    })
 
-    private uriSpec = {
+    private uriSpec = (restClient: RestClient<any>) => ({
         uri: (uri: string) => { 
-            this.url = uri;
-            return this.executeSpec;
+            restClient.url = uri;
+            return restClient.executeSpec(restClient);
          },
          graphs: (pathVariable?: string) => {
             const _pathVariable = pathVariable ? `/${pathVariable}` : "";
-            this.url = `/graphs${_pathVariable}`;
-            this.headers = { Authorization: "Bearer " + RestClient.jwtToken };
-            return this.executeSpec;
+            restClient.url = `/graphs${_pathVariable}`;
+            restClient.headers = { Authorization: "Bearer " + RestClient.jwtToken };
+            return restClient.executeSpec(restClient);
         },
         status: () => {
-            this.url = "/graph/status";
-            return this.executeSpec;
+            restClient.url = "/graph/status";
+            return restClient.executeSpec(restClient);
         },
         description: () => {
-            this.url = "/graph/config/description";
-            return this.executeSpec;
+            restClient.url = "/graph/config/description";
+            return restClient.executeSpec(restClient);
         },
         graphId: () => {
-            this.url = "/graph/config/graphId";
-            return this.executeSpec;
+            restClient.url = "/graph/config/graphId";
+            return restClient.executeSpec(restClient);
         },
         namespaces: () =>  {
-            this.url = "/namespaces";
-            this.headers = { Authorization: "Bearer " + RestClient.jwtToken };
-            return this.executeSpec;
+            restClient.url = "/namespaces";
+            restClient.headers = { Authorization: "Bearer " + RestClient.jwtToken };
+            return restClient.executeSpec(restClient);
         },
         authentication: (pathVariable?: string) => {
             const _pathVariable = pathVariable ? `/${pathVariable}` : "";
-            this.url = `/auth${_pathVariable}`;
-            return this.executeSpec;
+            restClient.url = `/auth${_pathVariable}`;
+            return restClient.executeSpec(restClient);
         },
         storeTypes: () => {
-            this.url="/storetypes"
-            this.headers = { Authorization: "Bearer " + RestClient.jwtToken };
-            return this.executeSpec;
+            restClient.url="/storetypes"
+            restClient.headers = { Authorization: "Bearer " + RestClient.jwtToken };
+            return restClient.executeSpec(restClient);
         }
-    }
+    })
 
-    private executeSpec = {
+    private executeSpec = (restClient: RestClient<any>) => ({
         execute: async () => {
             try {
                 const response: AxiosResponse<any> = await axios({
-                    baseURL: this.baseURL,
-                    url: this.url,
-                    method: this.method,
-                    headers: this.headers,
-                    data: this.data,
+                    baseURL: restClient.baseURL,
+                    url: restClient.url,
+                    method: restClient.method,
+                    headers: restClient.headers,
+                    data: restClient.data,
                 });
                 return RestClient.convert(response);
             } catch (e) {
                 throw RestClient.fromError(e);
             }
         }
-    }
+    })
 
     private static async convert(response: AxiosResponse<any>): Promise<IApiResponse> {
         return {
