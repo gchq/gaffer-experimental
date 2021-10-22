db3d7b74db306e9269498a24a1a97a95a86e4457
go159lab <Go159Lab@tutaNota.com>
2021-10-22 08:54:18 +0100

    Add more validation on add edge and add entity component

--- gaas-ui/src/components/schema-builder/add-edge.tsx
+++ gaas-ui/src/components/schema-builder/add-edge.tsx
@@ -1,144 +1,151 @@
-
-import React, {ReactElement, useState} from "react";
-import {Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
-import {useImmerReducer} from "use-immer"
+import React, { ReactElement, useState } from "react"
+import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@material-ui/core"
+import { useImmerReducer } from "use-immer"
 
 export default function AddEdge(): ReactElement {
+  const instialState = {
+    edgename: {
+      value: "",
+      hassErrors: false,
+      message: ""
+    },
 
+    edgeDescription: {
+      value: "",
+      hassErrors: false,
+      message: ""
+    },
 
-    const instialState = {
-        edgename: {
-            value: "",
-            hassErrors: false,
-            message: ""
-        },
+    edgeSource: {
+      value: "",
+      hassErrors: false
+    },
 
-        edgeDescription: {
-            value: "",
-            hassErrors: false,
-            message: ""
+    edgeDestnation: {
+      value: "",
+      hassErrors: false
+    },
+
+    entityDirected: {
+      value: "",
+      hassErrors: false
+    }
+  }
+
+  function ourReducer(draft: any, action: any) {
+    switch (action.type) {
+      case "validateEdgeName":
+        draft.edgename.hassErrors = false
+        draft.edgename.value = action.value
+        draft.edgename.message = ""
+
+        if (!/^[a-zA-Z]*$/.test(draft.edgename.value)) {
+          draft.edgename.hassErrors = true
+          draft.edgename.message = "Edge name can only contain letters"
         }
-    }
+        return
 
-    function ourReducer(draft:any, action: any) {
+      case "validateEdgeDescription":
+        draft.edgeDescription.hassErrors = false
+        draft.edgeDescription.value = action.value
+        draft.edgeDescription.message = ""
 
-        switch(action.type){
-            case "validateEdgeName":
-                draft.edgename.hassErrors = false
-                draft.edgename.value = action.value
-                draft.edgename.message = ""
-
-                if(!/^[a-zA-Z]*$/.test(draft.edgename.value)){
-                    draft.edgename.hassErrors = true
-                    draft.edgename.message = "Edge name can only contain letters"
-                }
-                return
-            case "validateEdgeDescription":
-                draft.edgeDescription.hassErrors = false
-                draft.edgeDescription.value = action.value
-                draft.edgeDescription.message=""
-               
-              if(draft.edgeDescription.value && !/^[a-zA-Z0-9\s]*$/.test(draft.edgeDescription.value)){
-                    draft.edgeDescription.hassErrors = true
-                    draft.edgeDescription.message = "Edge description can only contain alpha numeric letters and spaces"
-                }
-                return
+        if (draft.edgeDescription.value && !/^[a-zA-Z0-9\s]*$/.test(draft.edgeDescription.value)) {
+          draft.edgeDescription.hassErrors = true
+          draft.edgeDescription.message = "Edge description can only contain alpha numeric letters and spaces"
         }
+        return
+
+      case "validateEdgeSource":
+        draft.edgeSource.hassErrors = false
+        draft.edgeSource.value = action.value
+        if (draft.edgeSource.value.length === 0) {
+          draft.edgeSource.hassErrors = true
+        }
+        return
+
+      case "validateEdgeDestnation":
+        draft.edgeDestnation.hassErrors = false
+        draft.edgeDestnation.value = action.value
+        if (draft.edgeDestnation.value.length === 0) {
+          draft.edgeDestnation.hassErrors = true
+        }
+        return
+
+      case "validateEntityDirected":
+        draft.entityDirected.hassErrors = false
+        draft.entityDirected.value = action.value
+        if (draft.entityDirected.value.length === 0) {
+          draft.entityDirected.hassErrors = true
+        }
+        return
     }
+  }
 
-    const [state, dispatch] = useImmerReducer(ourReducer,instialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, instialState)
 
-  
-    function disableAddEdgeButton(): boolean {
-        return (
-            (state.edgename.value.length === 0 || state.edgename.hassErrors) ||
-            (state.edgeDescription.value.length === 0 || state.edgeDescription.hassErrors)
-        )
-    }
-  
-    
-    return (
-        <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            id={"add-edge-inputs"}
-        >
-            <TextField
-                id={"edge-name-input"}
-                label={"Edge Name"}
-                aria-label="edge-name-input"
-                inputProps={{
-                    name: "Edge Name",
-                    id: "edge-name-input",
-                    "aria-label": "edge-name-input"
-                }}
-                name={"edge-name"}
-                variant="outlined"
-                error={state.edgename.hassErrors}
-                required
-                onChange={(e) => dispatch({type: "validateEdgeName", value: e.target.value})}
-                helperText={state.edgename.message}
-            />
-            <TextField
-                id={"edge-description-input"}
-                label={"Description"}
-                aria-label="edge-description-input"
-                inputProps={{
-                    name: "Edge Description",
-                    id: "edge-description-input",
-                    "aria-label": "edge-description-input"
-                }}
-                name={"edge-description"}
-                variant="outlined"
-                error={state.edgeDescription.hassErrors}
-                required
-                onChange={(e) => dispatch({type: "validateEdgeDescription", value: e.target.value})}
-                helperText={state.edgeDescription.message}
-            />
-            <FormControl fullWidth id={"edge-source-formcontrol"}>
-                <InputLabel id="edge-source-select-label">Source</InputLabel>
-                <Select
-                    labelId="edge-source-select-label"
-                    id="edge-source-select"
-                    label="Source"
-                >
-                    <MenuItem value={"type 1"}>Type 1</MenuItem>
-                    <MenuItem value={"type 2"}>Type 2</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl fullWidth id={"edge-destination-formcontrol"}>
-                <InputLabel id="edge-destination-select-label">Destination</InputLabel>
-                <Select
-                    labelId="edge-destination-select-label"
-                    id="edge-destination-select"
-                    label="Destination"
-                >
-                    <MenuItem value={"type 1"}>Type 1</MenuItem>
-                    <MenuItem value={"type 2"}>Type 2</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl fullWidth id={"edge-directed-formcontrol"}>
-                <InputLabel id="edge-directed-select-label">Directed</InputLabel>
-                <Select
-                    labelId="edge-directed-select-label"
-                    id="edge-directed-select"
-                    label="Directed"
-                >
-                    <MenuItem value={"True"}>True</MenuItem>
-                    <MenuItem value={"False"}>False</MenuItem>
-                </Select>
-            </FormControl>
-            <Button 
-                id={"add-edge-button"}
-                name={"Add Edge"}
-                color="primary"
-                disabled={disableAddEdgeButton()}
-            >
-                Add Edge
-            </Button>
+  function disableAddEdgeButton(): boolean {
+    return state.edgename.value.length === 0 || state.edgename.hassErrors || state.edgeDescription.value.length === 0 || state.edgeDescription.hassErrors || state.edgeSource.value.length === 0 || state.edgeDestnation.value.length === 0 || state.entityDirected.value.length === 0
+  }
 
-        </Grid>
-);
+  return (
+    <Grid container direction="column" justify="center" alignItems="center" id={"add-edge-inputs"}>
+      <TextField
+        id={"edge-name-input"}
+        label={"Edge Name"}
+        aria-label="edge-name-input"
+        inputProps={{
+          name: "Edge Name",
+          id: "edge-name-input",
+          "aria-label": "edge-name-input"
+        }}
+        name={"edge-name"}
+        variant="outlined"
+        error={state.edgename.hassErrors}
+        required
+        onChange={(e) => dispatch({ type: "validateEdgeName", value: e.target.value })}
+        helperText={state.edgename.message}
+      />
+      <TextField
+        id={"edge-description-input"}
+        label={"Description"}
+        aria-label="edge-description-input"
+        inputProps={{
+          name: "Edge Description",
+          id: "edge-description-input",
+          "aria-label": "edge-description-input"
+        }}
+        name={"edge-description"}
+        variant="outlined"
+        error={state.edgeDescription.hassErrors}
+        required
+        onChange={(e) => dispatch({ type: "validateEdgeDescription", value: e.target.value })}
+        helperText={state.edgeDescription.message}
+      />
+      <FormControl fullWidth id={"edge-source-formcontrol"}>
+        <InputLabel id="edge-source-select-label">Source</InputLabel>
+        <Select labelId="edge-source-select-label" id="edge-source-select" label="Source" onChange={(e) => dispatch({ type: "validateEdgeSource", value: e.target.value })}>
+          <MenuItem value={"type 1"}>Type 1</MenuItem>
+          <MenuItem value={"type 2"}>Type 2</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth id={"edge-destination-formcontrol"}>
+        <InputLabel id="edge-destination-select-label">Destination</InputLabel>
+        <Select labelId="edge-destination-select-label" id="edge-destination-select" label="Destination" onChange={(e) => dispatch({ type: "validateEdgeDestnation", value: e.target.value })}>
+          <MenuItem value={"type 1"}>Type 1</MenuItem>
+          <MenuItem value={"type 2"}>Type 2</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth id={"edge-directed-formcontrol"}>
+        <InputLabel id="edge-directed-select-label">Directed</InputLabel>
+        <Select labelId="edge-directed-select-label" id="edge-directed-select" label="Directed" onChange={(e) => dispatch({ type: "validateEntityDirected", value: e.target.value })}>
+          <MenuItem value={"True"}>True</MenuItem>
+          <MenuItem value={"False"}>False</MenuItem>
+        </Select>
+      </FormControl>
+      <Button id={"add-edge-button"} name={"Add Edge"} color="primary" disabled={disableAddEdgeButton()}>
+        Add Edge
+      </Button>
+    </Grid>
+  )
 }
--- gaas-ui/src/components/schema-builder/add-entity.tsx
+++ gaas-ui/src/components/schema-builder/add-entity.tsx
@@ -1,115 +1,108 @@
-import React, {ReactElement, useState} from "react";
-import {Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
-import {useImmerReducer} from "use-immer"
+import React, { ReactElement, useState } from "react"
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@material-ui/core"
+import { useImmerReducer } from "use-immer"
 
 export default function AddEntity(): ReactElement {
+  const instialState = {
+    entityname: {
+      value: "",
+      hassErrors: false,
+      message: ""
+    },
 
-    const instialState = {
-        entityname: {
-            value: "",
-            hassErrors: false,
-            message: ""
-        },
+    entityDescription: {
+      value: "",
+      hassErrors: false,
+      message: ""
+    },
 
-        entityDescription: {
-            value: "",
-            hassErrors: false,
-            message: ""
+    entityVertex: {
+      value: "",
+      hassErrors: false
+    }
+  }
+
+  function ourReducer(draft: any, action: any) {
+    switch (action.type) {
+      case "validateEntityeName":
+        draft.entityname.hassErrors = false
+        draft.entityname.value = action.value
+        draft.entityname.message = ""
+        if (draft.entityname.value && !/^[a-zA-Z]*$/.test(draft.entityname.value)) {
+          draft.entityname.hassErrors = true
+          draft.entityname.message = "Entity name can only contain letters"
         }
-    }
-
-    function ourReducer(draft:any, action: any) {
-
-        switch(action.type){
-            case "validateEntityeName":
-                draft.entityname.hassErrors = false
-                draft.entityname.value = action.value
-                draft.entityname.message="";
-                if(draft.entityname.value && !/^[a-zA-Z]*$/.test(draft.entityname.value)){
-                    draft.entityname.hassErrors = true
-                    draft.entityname.message = "Entity name can only contain letters"
-                }
-                return
-            case "validateEntityDescription":
-                draft.entityDescription.hassErrors = false
-                draft.entityDescription.value = action.value
-                draft.entityDescription.message=""
-              if(draft.entityDescription.value  && !/^[a-zA-Z0-9\s]*$/.test(draft.entityDescription.value)){
-                    draft.entityDescription.hassErrors = true
-                    draft.entityDescription.message = "Entity description can only contain alpha numeric letters and spaces"
-                }
-                return
+        return
+      case "validateEntityDescription":
+        draft.entityDescription.hassErrors = false
+        draft.entityDescription.value = action.value
+        draft.entityDescription.message = ""
+        if (draft.entityDescription.value && !/^[a-zA-Z0-9\s]*$/.test(draft.entityDescription.value)) {
+          draft.entityDescription.hassErrors = true
+          draft.entityDescription.message = "Entity description can only contain alpha numeric letters and spaces"
         }
+        return
+
+      case "validateEntityVertex":
+        draft.entityVertex.hassErrors = false
+        draft.entityVertex.value = action.value
+        if (draft.entityVertex.value.length === 0) {
+          draft.entityVertex.hassErrors = true
+        }
+        return
     }
+  }
 
-    const [state, dispatch] = useImmerReducer(ourReducer,instialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, instialState)
 
-    function disableAddEntityButton(): boolean {
-        return (
-            (state.entityname.value.length === 0 || state.entityname.hassErrors) ||
-            (state.entityDescription.value.length === 0 || state.entityDescription.hassErrors)
-        )
-    }
-    
-    return (
-        <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            id={"add-entity-inputs"}
-        >
-            <TextField
-                id={"entity-name-input"}
-                label={"Entity Name"}
-                aria-label="entity-name-input"
-                inputProps={{
-                    name: "Entity Name",
-                    id: "entity-name-input",
-                    "aria-label": "entity-name-input"
-                }}
-                name={"entity-name"}
-                error={state.entityname.hassErrors}
-                required
-                autoFocus
-                onChange={(e) => dispatch({type: "validateEntityeName", value: e.target.value})}
-                helperText={state.entityname.message}
-            />
-            <TextField
-                id={"entity-description-input"}
-                label={"Description"}
-                aria-label="entity-description-input"
-                inputProps={{
-                    name: "Entity Description",
-                    id: "entity-description-input",
-                    "aria-label": "entity-description-input"
-                }}
-                name={"entity-description"}
-                error={state.entityDescription.hassErrors}
-                required
-                autoFocus
-                onChange={(e) => dispatch({type: "validateEntityDescription", value: e.target.value})}
-                helperText={state.entityDescription.message}
-            />
-            <FormControl fullWidth id={"entity-vertex-formcontrol"}>
-                <InputLabel id="entity-vertex-select-label">Vertex</InputLabel>
-                <Select
-                    labelId="entity-vertex-select-label"
-                    id="entity-vertex-select"
-                    label="Vertex"
-                >
-                    <MenuItem value={"type 1"}>Type 1</MenuItem>
-                    <MenuItem value={"type 2"}>Type 2</MenuItem>
-                </Select>
-            </FormControl>
-            <Button
-                id={"add-entity-button"}
-                name={"Add Entity"}
-                disabled={disableAddEntityButton()}
-            >
-                Add Entity
-            </Button>
+  function disableAddEntityButton(): boolean {
+    return state.entityname.value.length === 0 || state.entityname.hassErrors || state.entityDescription.value.length === 0 || state.entityDescription.hassErrors || state.entityVertex.value.length === 0 || state.entityVertex.hassErrors
+  }
 
-        </Grid>
-    );
+  return (
+    <Grid container direction="column" justify="center" alignItems="center" id={"add-entity-inputs"}>
+      <TextField
+        id={"entity-name-input"}
+        label={"Entity Name"}
+        aria-label="entity-name-input"
+        inputProps={{
+          name: "Entity Name",
+          id: "entity-name-input",
+          "aria-label": "entity-name-input"
+        }}
+        name={"entity-name"}
+        error={state.entityname.hassErrors}
+        required
+        autoFocus
+        onChange={(e) => dispatch({ type: "validateEntityeName", value: e.target.value })}
+        helperText={state.entityname.message}
+      />
+      <TextField
+        id={"entity-description-input"}
+        label={"Description"}
+        aria-label="entity-description-input"
+        inputProps={{
+          name: "Entity Description",
+          id: "entity-description-input",
+          "aria-label": "entity-description-input"
+        }}
+        name={"entity-description"}
+        error={state.entityDescription.hassErrors}
+        required
+        autoFocus
+        onChange={(e) => dispatch({ type: "validateEntityDescription", value: e.target.value })}
+        helperText={state.entityDescription.message}
+      />
+      <FormControl fullWidth id={"entity-vertex-formcontrol"}>
+        <InputLabel id="entity-vertex-select-label">Vertex</InputLabel>
+        <Select labelId="entity-vertex-select-label" id="entity-vertex-select" label="Vertex" onChange={(e) => dispatch({ type: "validateEntityVertex", value: e.target.value })}>
+          <MenuItem value={"type 1"}>Type 1</MenuItem>
+          <MenuItem value={"type 2"}>Type 2</MenuItem>
+        </Select>
+      </FormControl>
+      <Button id={"add-entity-button"} name={"Add Entity"} color="primary" disabled={disableAddEntityButton()}>
+        Add Entity
+      </Button>
+    </Grid>
+  )
 }
