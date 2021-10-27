2fd49166b5d3be0a4f5a655ae671472ec0f134ae
Rocky <rocky341@protonmail.com>
2021-10-27 11:57:10 +0100

    change regex

--- gaas-ui/babel.config.js
+++ gaas-ui/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
-  transformIgnorePatterns: ["node_modules/((d3-array)/)"]
+  transformIgnorePatterns: ["node_modules/(?!d3-array)"]
 }
