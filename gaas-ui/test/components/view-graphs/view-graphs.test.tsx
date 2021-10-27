4420126e6a6b59ca23d103ae73edf90921120f97
Rocky <rocky341@protonmail.com>
2021-10-27 08:16:28 +0100

    fix test

--- gaas-ui/test/components/view-graphs/view-graphs.test.tsx
+++ gaas-ui/test/components/view-graphs/view-graphs.test.tsx
@@ -6,7 +6,6 @@ import {Graph} from "../../../src/domain/graph";
 import {DeleteGraphRepo} from "../../../src/rest/repositories/delete-graph-repo";
 import {RestApiError} from "../../../src/rest/RestApiError";
 import {GraphType} from "../../../src/domain/graph-type";
-import {IStoreTypes} from "../../../src/rest/http-message-interfaces/response-interfaces";
 import {GetStoreTypesRepo} from "../../../src/rest/repositories/get-store-types-repo";
 import {GetAllGraphIdsRepo} from "../../../src/rest/repositories/gaffer/get-all-graph-ids-repo";
 import {act} from "@testing-library/react";
