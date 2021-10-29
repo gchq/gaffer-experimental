06b81905409465dea0d41359695e09c53fb11330
Rocky <rocky341@protonmail.com>
2021-10-29 12:00:02 +0100

    Tests for schema-builder-dialog

--- gaas-ui/test/components/create-graph/schema-builder-dialog.test.tsx
+++ gaas-ui/test/components/create-graph/schema-builder-dialog.test.tsx
@@ -1,9 +1,45 @@
+import React from "react";
 import {mount, ReactWrapper} from "enzyme";
 import SchemaBuilderDialog from "../../../src/components/create-graph/schema-builder-dialog";
 
 let wrapper: ReactWrapper;
+const onCreateSchemaMockCallBack = jest.fn();
+
 beforeEach(() => {
-    wrapper = mount(<SchemaBuilderDialog/>);
+    wrapper = mount(<SchemaBuilderDialog
+        elementsSchema={
+            {
+                edges:
+                    {
+                        "TestEdge": {
+                            "description": "test",
+                            "source": "A",
+                            "destination": "B",
+                            "directed": "true"
+                        },
+                    },
+                entities:
+                    {
+                        "TestEntity": {
+                            "description": "test description",
+                            "vertex": "B"
+                        }
+                    }
+            }
+        }
+
+        onCreateSchema={onCreateSchemaMockCallBack}
+        typesSchema={{
+            types: {
+                "name":
+                    {
+                        "description": "test description",
+                        "class": "test.class"
+                    },
+            }
+        }
+        }
+    />);
 });
 afterEach(() => {
     wrapper.unmount();
@@ -15,11 +51,17 @@ describe("Schema builder Dialog UI Component", () => {
 
             expect(schemaBuilderButton.text()).toBe("Schema Builder");
         })
-       //TODO: Dialog open test
+        it("should open dialog on schema builder button press", () => {
+            wrapper.find("button#schema-builder-button").simulate("click");
+            const addTypeButton = wrapper.find("button#add-type-button");
+            expect(addTypeButton.text()).toBe("Add Type");
 
+            const addEdgeButton = wrapper.find("button#add-edge-button");
+            expect(addEdgeButton.text()).toBe("Add Edge");
+
+            const addEntityButton = wrapper.find("button#add-entity-button");
+            expect(addEntityButton.text()).toBe("Add Entity");
+        })
     })
 
 })
-function clickSchemaBuilderButton() {
-    wrapper.find("button#schema-builder-button").simulate("click");
-}
