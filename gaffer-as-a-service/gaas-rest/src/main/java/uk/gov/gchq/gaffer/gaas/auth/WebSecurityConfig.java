b96345560b40cd13539c377d6d49c80001d8a399
go159lab <Go159Lab@tutaNota.com>
2021-11-04 12:19:37 +0000

    Add cognito jwt integration

--- gaffer-as-a-service/gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/auth/WebSecurityConfig.java
+++ gaffer-as-a-service/gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/auth/WebSecurityConfig.java
@@ -14,75 +14,34 @@
  * limitations under the License.
  */
 package uk.gov.gchq.gaffer.gaas.auth;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    @Autowired
-//    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//
-//    @Autowired
-//    private UserDetailsService jwtUserDetailsService;
-//
-//    @Autowired
-//    private JwtRequestFilter jwtRequestFilter;
-//
-//    public WebSecurityConfig() {
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
-//        // configure AuthenticationManager so that it knows from where to load
-//        // user for matching credentials
-//        // Use BCryptPasswordEncoder
-//        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 
-    @Override
-    protected void configure(final HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
-//        httpSecurity.csrf().disable()
-//                // dont authenticate this particular request
-//                .authorizeRequests()
-//                .antMatchers("/auth", "/v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources",
-//                        "/swagger-resources/**", "/webjars/**", "/actuator/**")
-//                .permitAll()
-//                // all other requests need to be authenticated
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                // make sure we use stateless session; session won't be used to
-//                // store user's state.
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        // Add a filter to validate the tokens with every request
-//        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//        httpSecurity.cors();
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        httpSecurity.authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry.anyRequest().authenticated()).oauth2ResourceServer().jwt();
-    }
+        @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+        String issuerUri;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authorizeRequests ->
+                            authorizeRequests
+                                    .antMatchers("/v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources",
+                      "/swagger-resources/**", "/webjars/**", "/actuator/**").hasAuthority("SCOPE_services:read")
+                                    .anyRequest().authenticated()
+                    )
+                    .oauth2ResourceServer(oauth2ResourceServer ->
+                            oauth2ResourceServer
+                                    .jwt(jwt ->
+                                            jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+                                    )
+                    );
+        }
+
 }
