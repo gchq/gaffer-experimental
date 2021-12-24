935db30e51ed05253bd7895493095fae6cf1d7a7
Rocky <rocky341@protonmail.com>
2021-12-24 12:52:57 +0000

    test update

--- gaffer-as-a-service/gaas-rest/src/test/java/uk/gov/gchq/gaffer/gaas/client/graph/AddGraphsOperationTest.java
+++ gaffer-as-a-service/gaas-rest/src/test/java/uk/gov/gchq/gaffer/gaas/client/graph/AddGraphsOperationTest.java
@@ -126,7 +126,7 @@ public class AddGraphsOperationTest {
 
         final GraphOperationException actual = assertThrows(GraphOperationException.class, () -> new AddGraphsOperation(url, SUB_GRAPHS).execute());
 
-        final String expected = "AddGraph OperationChain request to Federated Store Graph failed. Reason: Failed to resolve 'invalid-host' after 4 queries , at http://invalid-host:8080/rest/graph/operations/execute";
-        assertTrue(actual.getMessage().equals(expected));
+        final String expected = "AddGraph OperationChain request to Federated Store Graph failed. Reason: Failed to resolve 'invalid-host'";
+        assertTrue(actual.getMessage().contains(expected));
     }
 }
