374dda921428746f4c2b0ab61978eef41b4f0217
Rocky <rocky341@protonmail.com>
2022-01-04 12:43:07 +0000

    PR Changes

--- gaffer-as-a-service/gaas-rest/src/test/java/uk/gov/gchq/gaffer/gaas/integrationtests/CreateFederatedGraphIT.java
+++ gaffer-as-a-service/gaas-rest/src/test/java/uk/gov/gchq/gaffer/gaas/integrationtests/CreateFederatedGraphIT.java
@@ -38,6 +38,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class CreateFederatedGraphIT extends AbstractTest {
 
     private static final String VALID_ROOT = "/rest";
+    private static final String Graph_ID = "TEST_GRAPH_ID";
 
     @Autowired
     private ApiClient apiClient;
@@ -47,7 +48,7 @@ public class CreateFederatedGraphIT extends AbstractTest {
     @Test
     public void testAddGraphReturns201OnSuccess() throws Exception {
         final List<ProxySubGraph> subGraphs = Arrays.asList(new ProxySubGraph(proxyGraphId, proxyGraphHost, VALID_ROOT));
-        final GaaSCreateRequestBody federatedRequestBody = new GaaSCreateRequestBody("igraph", TEST_GRAPH_DESCRIPTION, "federated", subGraphs);
+        final GaaSCreateRequestBody federatedRequestBody = new GaaSCreateRequestBody(Graph_ID, TEST_GRAPH_DESCRIPTION, "federated", subGraphs);
 
         final MockHttpServletResponse response = mvc.perform(post("/graphs")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -63,7 +64,7 @@ public class CreateFederatedGraphIT extends AbstractTest {
     @Test
     public void whenSubGraphURLIsInvalid_shouldReturnBadRequest() throws Exception {
         final List<ProxySubGraph> subGraphs = Arrays.asList(new ProxySubGraph("TestGraph", "http://invalid.url", "/rest"));
-        final GaaSCreateRequestBody federatedRequestBody = new GaaSCreateRequestBody("igraph", TEST_GRAPH_DESCRIPTION, "federated", subGraphs);
+        final GaaSCreateRequestBody federatedRequestBody = new GaaSCreateRequestBody(Graph_ID, TEST_GRAPH_DESCRIPTION, "federated", subGraphs);
 
         final MockHttpServletResponse result = mvc.perform(post("/graphs")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -95,9 +96,8 @@ public class CreateFederatedGraphIT extends AbstractTest {
         final String group = "gchq.gov.uk"; // String | the custom resource's group
         final String version = "v1"; // String | the custom resource's version
         final String plural = "gaffers"; // String | the custom resource's plural name. For TPRs this would be lowercase plural kind.
-        final String name = "igraph"; // String | the custom object's name
         try {
-            apiInstance.deleteNamespacedCustomObject(group, version, namespace, plural, name, null, null, null, null, null);
+            apiInstance.deleteNamespacedCustomObject(group, version, namespace, plural, Graph_ID, null, null, null, null, null);
         } catch (Exception e) {
             // Do nothing
         }
