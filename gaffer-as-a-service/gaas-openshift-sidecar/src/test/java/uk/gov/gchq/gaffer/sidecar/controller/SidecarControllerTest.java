d195a48d668abea92923c547b9e899efcf4e531c
Rocky <rocky341@protonmail.com>
2022-04-04 14:58:30 +0100

    Checkstyle

--- gaffer-as-a-service/gaas-openshift-sidecar/src/test/java/uk/gov/gchq/gaffer/sidecar/controller/SidecarControllerTest.java
+++ gaffer-as-a-service/gaas-openshift-sidecar/src/test/java/uk/gov/gchq/gaffer/sidecar/controller/SidecarControllerTest.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2021 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.gaffer.sidecar.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 import uk.gov.gchq.gaffer.sidecar.util.UnitTest;
 
 @WebFluxTest(controllers = SidecarController.class)
@@ -15,7 +29,7 @@ public class SidecarControllerTest {
     private WebTestClient webClient;
 
     @Test
-    void whoamiEndpointReturns200WhenXEmailPresent(){
+    void whoamiEndpointReturns200WhenXEmailPresent() {
         final String xemail = "mytest@email.com";
         webClient.get()
                 .uri("/whoami")
