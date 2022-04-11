0bfd8e233bada61ece6cb4e947542ca6570fe783
K-itKat20 <k-itkat811@protonmail.com>
2022-04-11 13:09:44 +0100

    created dynamic login form + unit tests

---
+++ gaas-ui/src/components/login/dynamic-login-form.tsx
@@ -0,0 +1,75 @@
+import { Button, Container, createStyles, CssBaseline, Dialog, Typography } from "@material-ui/core";
+import React from "react";
+import ReusableTextField from "../reusable-components/reusable-text-field";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+interface IProps {
+    requiredFields: string[];
+    onClickSignIn(textFieldValues: Map<string, string>): void;
+}
+
+function styles(theme: any) {
+    return createStyles({
+        root: {
+            margin: 0,
+            padding: theme.spacing(2),
+        },
+        closeButton: {
+            position: "absolute",
+            right: theme.spacing(1),
+            top: theme.spacing(1),
+            color: theme.palette.grey[500],
+        },
+        large: {
+            width: theme.spacing(7),
+            height: theme.spacing(7),
+        },
+    });
+}
+
+interface IState {
+    requiredFields: string[];
+}
+export default function DynamicLoginForm(props: IProps) {
+    const { requiredFields, onClickSignIn } = props;
+    const textfieldValues = new Map<string, string>();
+    return (
+        <Container maxWidth="xs" aria-label="login-form" id="login-form">
+            <CssBaseline />
+            <div
+                style={{
+                    marginTop: "20px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                {requiredFields.map((field: string) => (
+                    <ReusableTextField
+                        name={field}
+                        onChange={(textFieldInput: string) => {
+                            textfieldValues.set(field, textFieldInput);
+                        }}
+                    />
+                ))}
+                <Button
+                    fullWidth
+                    id="submit-sign-in-button"
+                    aria-label="submit-sign-in-button"
+                    variant="contained"
+                    color="primary"
+                    style={{ marginTop: "20px" }}
+                    // disabled={this.disableSignInButton()}
+                    onClick={() => {
+                        onClickSignIn(textfieldValues);
+                    }}
+                >
+                    Sign In
+                </Button>
+            </div>
+        </Container>
+    );
+}
---
+++ gaas-ui/test/components/login/dynamic-login-form.test.tsx
@@ -0,0 +1,32 @@
+import { mount, ReactWrapper } from "enzyme";
+import DynamicLoginForm from "../../../src/components/login/dynamic-login-form";
+
+let component: ReactWrapper;
+const onClickSignIn = jest.fn();
+beforeEach(
+    () =>
+        (component = mount(
+            <DynamicLoginForm requiredFields={["username", "password"]} onClickSignIn={onClickSignIn} />
+        ))
+);
+afterEach(() => component.unmount());
+describe("Dynamic Login Modal", () => {
+    it("Should display the correct textfield based on the requiredField prop", () => {
+        expect(component.find("input#username").length).toBe(1);
+        expect(component.find("input#password").length).toBe(1);
+    });
+    it("should callback with the requiredFields and textFields as a map when submit is clicked", () => {
+        const expectedMap = new Map<string, string>();
+        expectedMap.set("username", "aTestUsername");
+        expectedMap.set("password", "aTestPassword");
+        component.find("input#username").simulate("change", {
+            target: { value: "aTestUsername" },
+        });
+        component.find("input#password").simulate("change", {
+            target: { value: "aTestPassword" },
+        });
+        component.find("button#submit-sign-in-button").simulate("click");
+        expect(onClickSignIn).toHaveBeenCalledTimes(1);
+        expect(onClickSignIn).toHaveBeenCalledWith(expectedMap);
+    });
+});
