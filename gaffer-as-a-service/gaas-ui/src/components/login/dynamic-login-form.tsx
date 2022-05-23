e14414d8f367cd3d7ae85db6f12332bd2267e01d
Rocky <rocky341@protonmail.com>
2022-05-23 10:06:48 +0100

    Copyright headers

--- gaffer-as-a-service/gaas-ui/src/components/login/dynamic-login-form.tsx
+++ gaffer-as-a-service/gaas-ui/src/components/login/dynamic-login-form.tsx
@@ -1,7 +1,21 @@
-import { Button, Container, createStyles, CssBaseline, Dialog, Typography } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+/*
+ * Copyright 2021-2022 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Button, Container, createStyles, CssBaseline, Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import ReusableTextField from "../reusable-components/reusable-text-field";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 interface IProps {
     requiredFields: Array<string>;
