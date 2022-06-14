763ebc818b1d00a74814dd98e8e844e5bd91396c
go159lab <Go159Lab@tutaNota.com>
2022-06-14 19:55:34 +0100

    Add more  test

--- gaffer-as-a-service/gaas-ui/test/components/add-collaborator/add-collaborator.test.tsx
+++ gaffer-as-a-service/gaas-ui/test/components/add-collaborator/add-collaborator.test.tsx
@@ -18,7 +18,9 @@ import { mount, ReactWrapper } from "enzyme";
 import React from "react";
 import { act } from "react-dom/test-utils";
 import AddCcollaborator from "../../../src/components/add-collaborator/add-collaborator";
+import { AddCollaboratorRepo } from "../../../src/rest/repositories/add-collaborator-repo";
 
+jest.mock("../../../src/rest/repositories/add-collaborator-repo");
 jest.mock("react-router-dom", () => ({
     ...jest.requireActual("react-router-dom"),
     useLocation: () => ({ state: { graphId: "graphId" } }),
@@ -31,21 +33,70 @@ beforeEach(async () => {
 });
 
 afterEach(() => {
+    wrapper.unmount();
     jest.resetAllMocks();
 });
 
 describe("AddCollaborator UI component", () => {
     describe("Layout On Render", () => {
         it("Should have a Graph Id, username inputs", () => {
-            const graphIdTextfield = wrapper.find("input");
-            expect(graphIdTextfield.at(0).props().name).toBe("graph-id");
+            const graphId = wrapper.find("input#graph-id-input");
+            expect(graphId.props().value).toBe("graphId");
 
-            const usernameTextfield = wrapper.find("input");
-            expect(usernameTextfield.at(1).props().name).toBe("User Name");
+            const username = wrapper.find("input#username-input");
+            expect(username.props().name).toBe("User Name");
+        });
+
+        it("should capture username correctly onChange", () => {
+            const username = wrapper.find("input#username-input");
+            inputUsername("John");
+            // expect(graphId.props().value).toBe("John");
+            expect(username.instance().value).toBe("John");
+        });
+
+        it("should be disabled when username field is empty", () => {
+            inputUsername("");
+
+            expect(wrapper.find("button#add-collaborator-button").props().disabled).toBe(true);
         });
         it("should have a Submit button", () => {
             const submitButton = wrapper.find("button#add-collaborator-button").text();
             expect(submitButton).toBe("Add Collaborator");
         });
     });
+
+    describe("On Submit Request", () => {
+        it("should display success message in the NotificationAlert", async () => {
+            const mock = jest.fn();
+            mockAddCollaboratorRepoWithFunction(mock);
+            await inputUsername("John");
+            await wrapper.update();
+            await wrapper.update();
+            await clickSubmit();
+            await wrapper.update();
+            await wrapper.update();
+           expect(wrapper.find("div#notification-alert").text()).toBe("graphId was successfully added collaborator");
+            expect(mock).toHaveBeenLastCalledWith("graphId", "John");
+      
+        });
+    });
 });
+
+async function clickSubmit(): Promise<void> {
+    await act(async () => {
+        wrapper.find("button#add-collaborator-button").simulate("click");
+    });
+}
+
+function inputUsername(username: string): void {
+    wrapper.find("input#username-input").simulate("change", {
+        target: { value: username },
+    });
+}
+
+function mockAddCollaboratorRepoWithFunction(f: () => void): void {
+    // @ts-ignore
+    AddCollaboratorRepo.mockImplementationOnce(() => ({
+        addCollaborator: f,
+    }
+}
