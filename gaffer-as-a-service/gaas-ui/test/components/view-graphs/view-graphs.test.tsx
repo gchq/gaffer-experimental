23c18639e821c4842ff77a8d9d753ca3e2598786
go159lab <Go159Lab@tutaNota.com>
2022-06-15 14:13:32 +0100

    Fix failing test

--- gaffer-as-a-service/gaas-ui/test/components/view-graphs/view-graphs.test.tsx
+++ gaffer-as-a-service/gaas-ui/test/components/view-graphs/view-graphs.test.tsx
@@ -59,7 +59,7 @@ describe("When ViewGraphs mounts", () => {
         await component.update();
 
         expect(component.find("thead").text()).toBe(
-            "Graph IDStore TypeStatusUI URLREST URLGraph Auto Destroy DateActionsAdd CollaboratorActions"
+            "Graph IDStore TypeStatusUI URLREST URLAdd CollaboratorGraph Auto Destroy DateActions"
         );
         expect(component.find("tbody").text()).toBe(
             "testId1 MUPhttp://testId-1.app/uihttp://testId-1.app/rest2022-06-09t15:55:34.006"
