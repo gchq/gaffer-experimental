9f0144b1b65ea39933ac50ba46b3a4b5f81a8b9f
Rocky <rocky341@protonmail.com>
2022-06-27 16:05:49 +0100

    deleteCollaborator tests

--- gaffer-as-a-service/gaas-ui/test/components/view-collaborators/view-collaborators.test.tsx
+++ gaffer-as-a-service/gaas-ui/test/components/view-collaborators/view-collaborators.test.tsx
@@ -16,22 +16,13 @@
 
 import React from "react";
 import { mount, ReactWrapper } from "enzyme";
-import ViewGraph from "../../../src/components/view-graphs/view-graphs";
-import { GetAllGraphsRepo } from "../../../src/rest/repositories/get-all-graphs-repo";
-import { Graph } from "../../../src/domain/graph";
-import { DeleteGraphRepo } from "../../../src/rest/repositories/delete-graph-repo";
 import { APIError } from "../../../src/rest/APIError";
-import { GraphType } from "../../../src/domain/graph-type";
-import { GetStoreTypesRepo, IStoreTypes } from "../../../src/rest/repositories/get-store-types-repo";
-import { GetAllGraphIdsRepo } from "../../../src/rest/repositories/gaffer/get-all-graph-ids-repo";
-import { act } from "@testing-library/react";
 import { GraphCollaborator } from "../../../src/domain/graph-collaborator";
 import ViewCollaborators from "../../../src/components/view-collaborators/view-collaborators";
 import { GetAllGraphCollaboratorsRepo } from "../../../src/rest/repositories/get-all-graph-collaborators-repo";
+import { DeleteCollaboratorRepo } from "../../../src/rest/repositories/delete-collaborator-repo";
 
 jest.mock("../../../src/rest/repositories/get-all-graph-collaborators-repo");
-
-const mockedUsedNavigate = jest.fn();
 
 jest.mock("react-router-dom", () => ({
     ...jest.requireActual("react-router-dom"),
@@ -94,269 +85,37 @@ describe("When ViewCollaborators mounts", () => {
             "Failed to get all graph collaborators. Client Error: 404 Not Found"
         );
     });
-    // it("should not display Error AlertNotification when GetGraphs request successful", async () => {
-    //     mockGetGraphsToReturn([
-    //         new Graph(
-    //             "roadTraffic",
-    //             "DEPLOYED",
-    //             "http://roadTraffic.graph/ui",
-    //             "http://roadTraffic.graph/rest",
-    //             "UP",
-    //             "mapStore",
-    //             "2022-06-09t15:55:34.006",
-    //             GraphType.GAAS_GRAPH
-    //         ),
-    //     ]);
+    it("should delete collaborator when delete collaborator button click", async () => {
+        mockGetAllGraphCollaboratorsToReturn([new GraphCollaborator("myGraph", "myUser")]);
+        DeleteCollaboratorRepo.prototype.delete = jest.fn();
+        const wrapper: ReactWrapper = mount(
+            <ViewCollaborators
+                graphId={"myGraph"}
+                errorMessage={"someMessage"}
+                graphCollaborators={[new GraphCollaborator("myGraph", "myUser")]}
+            />
+        );
+        await wrapper.update();
+        await wrapper.update();
 
-    //     const component = mount(<ViewGraph />);
-    //     await component.update();
+        wrapper.find("tbody").find("button#view-collaborator-delete-button-0").simulate("click");
+        expect(DeleteCollaboratorRepo.prototype.delete).toHaveBeenLastCalledWith("myGraph", "myUser");
+    });
 
-    //     const table = component.find("table");
-    //     expect(table).toHaveLength(1);
-    //     expect(table.find("tbody").text()).toBe(
-    //         "roadTraffic MUPhttp://roadTraffic.graph/uihttp://roadTraffic.graph/rest2022-06-09t15:55:34.006"
-    //     );
-    //     expect(component.find("div#notification-alert").length).toBe(0);
-    // });
-    // it("should display a down status in the status column when the graph status is down", async () => {
-    //     mockGetGraphsToReturn([
-    //         new Graph(
-    //             "roadTraffic",
-    //             "DEPLOYED",
-    //             "http://roadTraffic.graph/ui",
-    //             "http://roadTraffic.graph/rest",
-    //             "DOWN",
-    //             "mapStore",
-    //             "2022-06-09t15:55:34.006",
-    //             GraphType.GAAS_GRAPH
-    //         ),
-    //     ]);
+    function mockGetAllGraphCollaboratorsToReturn(graphCollaborators: GraphCollaborator[]): void {
+        // @ts-ignore
+        GetAllGraphCollaboratorsRepo.mockImplementationOnce(() => ({
+            getAll: (graphId: string) =>
+                new Promise((resolve) => {
+                    resolve(graphCollaborators);
+                }),
+        }));
+    }
 
-    //     const component = mount(<ViewGraph />);
-    //     await component.update();
-
-    //     const table = component.find("table");
-    //     expect(table.find("tbody").text()).toBe(
-    //         "roadTraffic MDOWNhttp://roadTraffic.graph/uihttp://roadTraffic.graph/rest2022-06-09t15:55:34.006"
-    //     );
-    // });
+    function mockGetAllGraphCollaboratorsThrowsError(f: () => void): void {
+        // @ts-ignore
+        GetAllGraphCollaboratorsRepo.mockImplementationOnce(() => ({
+            getAll: f,
+        }));
+    }
 });
-
-// describe("Integration with GetAllGraphIds repo", () => {
-//     let component: ReactWrapper;
-//     afterEach(() => {
-//         component.unmount();
-//     });
-// it("should display federated graph ids as a list of strings", async () => {
-//     await mockGetStoreTypesRepoToReturn({
-//         storeTypes: ["accumulo", "mapStore", "proxy", "proxyNoContextRoot"],
-//         federatedStoreTypes: ["federated"],
-//     });
-//     await mockGetGraphsToReturn([
-//         new Graph(
-//             "apples",
-//             "ACTIVE",
-//             "http://apples.graph/ui",
-//             "http://apples.graph/rest",
-//             "UP",
-//             "federated",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//         new Graph(
-//             "pears",
-//             "DELETION IN PROGRESS",
-//             "http://pears.graph/ui",
-//             "http://pears.graph/rest",
-//             "UP",
-//             "mapStore",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//     ]);
-//     await act(async () => {
-//         component = mount(<ViewGraph />);
-//     });
-//     await act(async () => {
-//         await mockGetAllGraphIdsRepoToReturn(["accumulo-graph-1", "accumulo-graph-2"]);
-//     });
-
-//     await component.update();
-//     await component.update();
-//     await clickExpandRow(component);
-//     expect(component.find("tr#federated-graph-ids-0").text()).toBe(
-//         "Federated Graphs: accumulo-graph-1, accumulo-graph-2"
-//     );
-// });
-// it("should not display any graph ids when GetAllGraphIds returns and empty array", async () => {
-//     await mockGetStoreTypesRepoToReturn({
-//         storeTypes: ["accumulo", "mapStore", "proxy", "proxyNoContextRoot"],
-//         federatedStoreTypes: ["federated"],
-//     });
-//     await mockGetGraphsToReturn([
-//         new Graph(
-//             "apples",
-//             "ACTIVE",
-//             "http://apples.graph/ui",
-//             "http://apples.graph/rest",
-//             "UP",
-//             "federated",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//         new Graph(
-//             "pears",
-//             "DELETION IN PROGRESS",
-//             "http://pears.graph/ui",
-//             "http://pears.graph/rest",
-//             "UP",
-//             "mapStore",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//     ]);
-//     await act(async () => {
-//         component = mount(<ViewGraph />);
-//     });
-//     await act(async () => {
-//         await mockGetAllGraphIdsRepoToReturn([]);
-//     });
-
-//     await component.update();
-//     await component.update();
-//     await clickExpandRow(component);
-//     expect(component.find("tr#federated-graph-ids-0").text()).toBe("No Federated Graphs");
-// });
-// it("should display an error if GetAllGraphIds throws an error when called", async () => {
-//     await act(async () => {
-//         mockGetAllGraphIdsRepoThrowsError(() => {
-//             throw new APIError("Server Error", "Timeout exception");
-//         });
-//     });
-//     mockGetStoreTypesRepoToReturn({
-//         storeTypes: ["accumulo", "mapStore", "proxy", "proxyNoContextRoot"],
-//         federatedStoreTypes: ["federated"],
-//     });
-//     mockGetGraphsToReturn([
-//         new Graph(
-//             "apples",
-//             "ACTIVE",
-//             "http://apples.graph/ui",
-//             "http://apples.graph/rest",
-//             "UP",
-//             "federated",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//     ]);
-//     component = mount(<ViewGraph />);
-
-//     await component.update();
-//     await component.update();
-//     await component.update();
-
-//     clickExpandRow(component);
-//     expect(component.find("tr#federated-graph-ids-0").text()).toBe(
-//         "Federated Graphs: [GetAllGraphIds Operation - Server Error: Timeout exception]"
-//     );
-// });
-// it("should not display the row and execute GetALlGraphIds if graph is not Federated Store", async () => {
-//     await mockGetStoreTypesRepoToReturn({
-//         storeTypes: ["accumulo", "mapStore", "proxy", "proxyNoContextRoot"],
-//         federatedStoreTypes: ["federated"],
-//     });
-//     await mockGetGraphsToReturn([
-//         new Graph(
-//             "apples",
-//             "ACTIVE",
-//             "http://apples.graph/ui",
-//             "http://apples.graph/rest",
-//             "UP",
-//             "mapStore",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//         new Graph(
-//             "pears",
-//             "DELETION IN PROGRESS",
-//             "http://pears.graph/ui",
-//             "http://pears.graph/rest",
-//             "UP",
-//             "mapStore",
-//             "2022-06-09t15:55:34.006",
-//             GraphType.GAAS_GRAPH
-//         ),
-//     ]);
-//     await act(async () => {
-//         component = mount(<ViewGraph />);
-//     });
-
-//     await component.update();
-//     await component.update();
-//     await clickExpandRow(component);
-//     expect(component.find("tr#federated-graph-ids-0").length).toBe(0);
-// });
-//});
-
-async function clickExpandRow(component: ReactWrapper) {
-    component.find("button#expand-row-button-0").simulate("click");
-    await component.update();
-}
-
-async function mockGetAllGraphIdsRepoToReturn(graphIds: string[]) {
-    // @ts-ignore
-    GetAllGraphIdsRepo.mockImplementationOnce(() => ({
-        get: () =>
-            new Promise((resolve) => {
-                resolve(graphIds);
-            }),
-    }));
-}
-
-function mockDeleteGraphRepoToThrowError(f: () => void) {
-    // @ts-ignore
-    DeleteGraphRepo.mockImplementationOnce(() => ({
-        delete: f,
-    }));
-}
-
-function mockGetAllGraphCollaboratorsToReturn(graphCollaborators: GraphCollaborator[]): void {
-    // @ts-ignore
-    GetAllGraphCollaboratorsRepo.mockImplementationOnce(() => ({
-        getAll: (graphId: string) =>
-            new Promise((resolve) => {
-                resolve(graphCollaborators);
-            }),
-    }));
-}
-
-function mockGetAllGraphCollaboratorsThrowsError(f: () => void): void {
-    // @ts-ignore
-    GetAllGraphCollaboratorsRepo.mockImplementationOnce(() => ({
-        getAll: f,
-    }));
-}
-
-async function mockGetStoreTypesRepoToReturn(storetypes: IStoreTypes) {
-    // @ts-ignore
-    GetStoreTypesRepo.mockImplementationOnce(() => ({
-        get: () =>
-            new Promise((resolve) => {
-                resolve(storetypes);
-            }),
-    }));
-}
-
-async function mockGetStoreTypesRepoToThrow(f: () => void) {
-    // @ts-ignore
-    GetStoreTypesRepo.mockImplementationOnce(() => ({
-        get: f,
-    }));
-}
-
-async function mockGetAllGraphIdsRepoThrowsError(f: () => void) {
-    // @ts-ignore
-    GetAllGraphIdsRepo.mockImplementationOnce(() => ({
-        get: f,
-    }));
-}
